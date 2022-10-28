cb36c9cbcb0734eccceb4d29d0449426af83aab5
mcandemir <mhmtcndmr01@gmail.com>
2022-10-28 14:18:28 +0300

    parameterized service address

--- WebAPI/app.py
+++ WebAPI/app.py
@@ -1,8 +1,13 @@
+import argparse
 import requests
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--service', help='model api service address')
+args = parser.parse_args()
 
 
 @app.route('/')
@@ -13,7 +18,7 @@ def index():
 @app.route('/', methods=['POST'])
 def index_post():
     mail = request.form['mail']
-    r = requests.post("http://spamdetector-modelapi-service:5001/detect", data=mail)
+    r = requests.post(args.service, data=mail)
     return render_template('index.html', mail=r.text)
 
 
